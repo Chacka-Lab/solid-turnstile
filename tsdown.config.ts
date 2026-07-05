9a38690e116bfe2840aed1f0da13f8d5faadd7c4
YanAnghelp <yananghelp@gmail.com>
2026-07-05 23:27:12 +0800

    fix: JSX has not been converted

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'tsdown';
+import babel from '@rolldown/plugin-babel';
 
 export default defineConfig({
   dts: true,
   entry: ['./src/validate.ts', './src/component.tsx'],
   target: 'es2020',
+  plugins: [
+    babel({
+      presets: [['babel-preset-solid', { generate: 'dom' }]],
+    }),
+  ],
 });
